068ded0057e609057cfe4fcfe419115f13a76936
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-04-01 18:48:36 -0300

    fix: footer align items

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col items-center justify-center gap-4 border-t-2 bg-background  px-6 py-4 md:flex-row md:justify-between lg:px-10">
+    <footer className="flex w-full flex-col items-start justify-center gap-4 border-t-2 bg-background  px-6 py-4 md:flex-row md:justify-between lg:px-10">
       <div className="space-y-4 text-center md:text-left">
         <p className="font-semibold">Developed by Nayark</p>
         <p className="font-semibold">Contact: matheus.lima@ccc.ufcg.edu.br</p>
